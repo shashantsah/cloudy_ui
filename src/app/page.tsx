c986ef20fa49b43d1e572a91e0d7696c4b2872f4
shashantsah <shashant.sah@gmail.com>
2025-05-20 20:32:10 +0530

    get started button on the root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
 import Image from "next/image";
+import { Button } from "@/components/Button";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
    <div className="bg-red-500 text-white p-4 text-center">
-      If you can see this with red background and white text, Tailwind is working.
+      <Button variant="outline" className="mt-4">
+        <Link href="/auth">
+        Get Started
+        </Link>
+        </Button>       
     </div>
   );
 }
